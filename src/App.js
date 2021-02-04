ebccf0c540898bde96eb6ddbfecc9ac6b210859d
Sjouke91 <s.bosma1991@gmail.com>
2021-02-04 16:15:39 +0100

    Setup react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./pages/login/Login";
+
+import { Switch, Route } from "react-router-dom";
+import WorkoutList from "./pages/workoutList/WorkoutList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route exact path="/workoutlist" component={WorkoutList} />
+      </Switch>
     </div>
   );
 }
